01d76d7e0f4a88f3c0858e673c3755458529d763
Jailene <jaileneveras8@gmail.com>
2023-03-09 19:22:09 -0500

    bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Post = require ('./Post.js')
 
 const userSchema = new Schema({
