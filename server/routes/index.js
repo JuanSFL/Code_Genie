ed71a0e7ef620d891d74286b96fa45afdceb1e88
Bohrzz <113268086+Bohrzz@users.noreply.github.com>
2023-03-15 19:37:16 -0400

    updated code for changes in develop

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 // Import our modular routers for /openai
-const openAiRouter = require('/openai-route');
+const openAiRouter = require('./openai-route');
 
 
 const app = express();
 
-app.use('/openai-route', openAiRouter);
+app.use('./openai-route', openAiRouter);
 
 
 module.exports = app;
