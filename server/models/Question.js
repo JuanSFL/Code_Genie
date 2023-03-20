2a4655b5620ee8236e109e31033000fd1a069f8f
Jailene <jaileneveras8@gmail.com>
2023-03-20 14:47:29 -0400

    added questions model, created questionsseeds.json and added it to resolvers and typedefs

---
+++ server/models/Question.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+
+const questionSchema = new Schema({
+    questionText: {
+      type: String,
+      required: 'You need to leave a question!',
+      minlength: 1,
+      maxlength: 280,
+      trim: true,
+    },
+    questionAuthor: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    comments: [
+      {
+        commentText: {
+          type: String,
+          required: true,
+          minlength: 1,
+          maxlength: 280,
+        },
+        commentAuthor: {
+          type: String,
+          required: true,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+          get: (timestamp) => dateFormat(timestamp),
+        },
+      },
+    ],
+  });
+  
+  const Question = model('Question', questionSchema);
+  
+  module.exports = Question;
+  
